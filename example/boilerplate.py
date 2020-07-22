60c8cd6acaa94a77744bff34308459453e151bc7
husein zolkepli <husein.zol05@gmail.com>
2020-07-22 20:43:15 +0800

    pure=False for dask

---
+++ example/boilerplate.py
@@ -0,0 +1,4 @@
+def inc(x):
+    if x > 20:
+        raise Exception('x > 20')
+    return x + 1
--- waterhealer/dask.py
+++ waterhealer/dask.py
@@ -11,5 +11,37 @@ from . import core
 
 
 @DaskStream.register_api()
+class map(DaskStream):
+    def __init__(self, upstream, func, *args, **kwargs):
+        self.func = func
+        self.kwargs = kwargs
+        self.args = args
+
+        DaskStream.__init__(self, upstream)
+
+    def update(self, x, who = None):
+        client = default_client()
+        result = client.submit(
+            self.func, x, *self.args, **self.kwargs, pure = False
+        )
+        return self._emit(result)
+
+
+@DaskStream.register_api()
+class starmap(DaskStream):
+    def __init__(self, upstream, func, **kwargs):
+        self.func = func
+        stream_name = kwargs.pop('stream_name', None)
+        self.kwargs = kwargs
+
+        DaskStream.__init__(self, upstream, stream_name = stream_name)
+
+    def update(self, x, who = None):
+        client = default_client()
+        result = client.submit(apply, self.func, x, self.kwargs, pure = False)
+        return self._emit(result)
+
+
+@DaskStream.register_api()
 class partition_time(DaskStream, core.partition_time):
     pass
